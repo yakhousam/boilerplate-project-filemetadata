f3482f7266ba81052f4022186f556dd9a2d42b68
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-03-30 12:09:23 +0100

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+
 
 // require and use "multer"...
+const multer = require('multer');
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+// const upload = multer({
+//   dest: 'upload',
+//   limits: {fileSize: 1024 * 1024 * 10}
+// })
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +25,18 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+ if(req.file){
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+ }else{
+   res.redirect('/')
+ }
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
